f58e35a2e5694ffe37fa5a4ddc2d03f7cd10cc4b
tytyne <dusaflora2@gmail.com>
2019-02-12 15:24:09 +0200

    aading username and password in  credential

--- credentials.py
+++ credentials.py
@@ -1,42 +1,15 @@
 
-class credentials
-   password_list = []
-  def get_password(self):
-    password.password_list.append(self)
-    def __init__(self,psswrd):
-  self.psswrd = psswrd
 
-psswrd = "demo123"
+class credentials:
 
 
+    credential_list = [] # Empty user list
+
+    def __init__(self,username,pas):
+        self.username="admin"
+        self.pas="admin123"
 
 
 
-# flag = 0
-# while True:   
-#     if (len(psswrd)<8): 
-#         flag = -1
-#         break
-#     elif not re.search("[a-z]", psswrd): 
-#         flag = -1
-#         break
-#     elif not re.search("[A-Z]", psswrd): 
-#         flag = -1
-#         break
-#     elif not re.search("[0-9]", psswrd): 
-#         flag = -1
-#         break
-#     elif not re.search("[_@$]", psswrd): 
-#         flag = -1
-#         break
-#     elif re.search("\s", psswrd): 
-#         flag = -1
-#         break
-#     else: 
-#         flag = 0
-#         print("Valid Password") 
-#         break
-  
-# if flag ==-1: 
-#     print("Not a Valid Password") 
+
     
