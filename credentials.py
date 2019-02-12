b12b3531a461835278049bb8fec4d96a2b632530
tytyne <dusaflora2@gmail.com>
2019-02-12 14:56:52 +0200

    save changes

--- credentials.py
+++ credentials.py
@@ -7,39 +7,37 @@ class credentials
     def __init__(self,psswrd):
   self.psswrd = psswrd
 
- def delete_password(self):
-
- password.password_list.remove(self)
-
-
-
-
-
 psswrd = "demo123"
-flag = 0
-while True:   
-    if (len(psswrd)<8): 
-        flag = -1
-        break
-    elif not re.search("[a-z]", psswrd): 
-        flag = -1
-        break
-    elif not re.search("[A-Z]", psswrd): 
-        flag = -1
-        break
-    elif not re.search("[0-9]", psswrd): 
-        flag = -1
-        break
-    elif not re.search("[_@$]", psswrd): 
-        flag = -1
-        break
-    elif re.search("\s", psswrd): 
-        flag = -1
-        break
-    else: 
-        flag = 0
-        print("Valid Password") 
-        break
+
+
+
+
+
+# flag = 0
+# while True:   
+#     if (len(psswrd)<8): 
+#         flag = -1
+#         break
+#     elif not re.search("[a-z]", psswrd): 
+#         flag = -1
+#         break
+#     elif not re.search("[A-Z]", psswrd): 
+#         flag = -1
+#         break
+#     elif not re.search("[0-9]", psswrd): 
+#         flag = -1
+#         break
+#     elif not re.search("[_@$]", psswrd): 
+#         flag = -1
+#         break
+#     elif re.search("\s", psswrd): 
+#         flag = -1
+#         break
+#     else: 
+#         flag = 0
+#         print("Valid Password") 
+#         break
   
-if flag ==-1: 
-    print("Not a Valid Password") 
+# if flag ==-1: 
+#     print("Not a Valid Password") 
+    
